3f7bae9027e9c5dfdb4cfc9b3add2ad516bbeea3
silentfl <silentfl@mail.ru>
2017-06-16 11:47:52 +0300

    Bugs fix

--- horse/chessy.go
+++ horse/chessy.go
@@ -14,18 +14,18 @@ var (
 	isCoords = regexp.MustCompile("^[a-h][1-8]$").MatchString
 
 	horseSteps = []Coordinate{
-		Coordinate{1, -2},
-		Coordinate{2, -1},
-		Coordinate{2, 1},
 		Coordinate{1, 2},
-		Coordinate{-1, 2},
-		Coordinate{-2, 1},
-		Coordinate{-2, -1},
+		Coordinate{2, 1},
+		Coordinate{2, -1},
+		Coordinate{1, -2},
 		Coordinate{-1, -2},
+		Coordinate{-1, 2},
+		Coordinate{-2, -1},
+		Coordinate{-2, 1},
 	}
 )
 
-func NotToCoords(coords string) (c Coordinate, err error) {
+func NotationToCoords(coords string) (c Coordinate, err error) {
 	if !isCoords(coords) {
 		err = errors.New("Wrong coordinate")
 		return
@@ -34,12 +34,16 @@ func NotToCoords(coords string) (c Coordinate, err error) {
 	return Coordinate{int(coords[0] - 'a'), int(coords[1] - '1')}, nil
 }
 
+func CoordsToNotation(c Coordinate) string {
+	return fmt.Sprintf("%c%d ", c.X+97, c.Y+1)
+}
+
 func HorseMoves(coords Coordinate) (res []string) {
 	for _, delta := range horseSteps {
-		new_x, new_y := coords.X+delta.X, coords.Y+delta.Y
-		if new_x >= 0 && new_x <= 7 &&
-			new_y >= 0 && new_y <= 7 {
-			res = append(res, fmt.Sprintf("%c%d ", new_x+97, new_y))
+		new_coords := Coordinate{coords.X + delta.X, coords.Y + delta.Y}
+		if new_coords.X >= 0 && new_coords.X <= 7 &&
+			new_coords.Y >= 0 && new_coords.Y <= 7 {
+			res = append(res, CoordsToNotation(new_coords))
 		}
 	}
 
--- horse/chessy_test.go
+++ horse/chessy_test.go
@@ -5,30 +5,30 @@ import (
 	"testing"
 )
 
-func Test_NotToCoords(t *testing.T) {
-	tests := []struct {
-		input         string
-		expectedError bool
-		X, Y          int
-	}{
-		{"a1", false, 0, 0},
-		{"b1", false, 1, 0},
-		{"c1", false, 2, 0},
-		{"a8", false, 0, 7},
-		{"d4", false, 3, 3},
-		{"h1", false, 7, 0},
-		{"h8", false, 7, 7},
+var tests_for_coordinates = []struct {
+	input         string
+	expectedError bool
+	X, Y          int
+}{
+	{"a1", false, 0, 0},
+	{"b1", false, 1, 0},
+	{"c1", false, 2, 0},
+	{"a8", false, 0, 7},
+	{"d4", false, 3, 3},
+	{"h1", false, 7, 0},
+	{"h8", false, 7, 7},
 
-		{"", true, 0, 0},
-		{"a", true, 0, 0},
-		{"a9", true, 0, 0},
-		{"h98", true, 0, 0},
-		{"k8", true, 0, 0},
-		{"123", true, 0, 0},
-	}
+	{"", true, 0, 0},
+	{"a", true, 0, 0},
+	{"a9", true, 0, 0},
+	{"h98", true, 0, 0},
+	{"k8", true, 0, 0},
+	{"123", true, 0, 0},
+}
 
-	for _, test := range tests {
-		c, err := NotToCoords(test.input)
+func Test_NotationToCoords(t *testing.T) {
+	for _, test := range tests_for_coordinates {
+		c, err := NotationToCoords(test.input)
 		if test.expectedError && err == nil || !test.expectedError && err != nil {
 			t.Fatal(err)
 		}
@@ -43,7 +43,8 @@ func Test_HorseMoves(t *testing.T) {
 		coord  string
 		result []string
 	}{
-		{"a1", []string{"a2", "b1"}},
+		{"a1", []string{"b3", "c2"}},
+		{"h8", []string{"g6", "f7"}},
 		{"b1", []string{"c3", "d2", "a3"}},
 		{"b7", []string{"d8", "d6", "c5", "a5"}},
 		{"c1", []string{"d3", "e2", "a2", "b3"}},
@@ -55,7 +56,7 @@ func Test_HorseMoves(t *testing.T) {
 	}
 
 	for i, test := range tests {
-		coord, _ := NotToCoords(test.coord)
+		coord, _ := NotationToCoords(test.coord)
 		result := HorseMoves(coord)
 		if reflect.DeepEqual(result, test.result) {
 			t.Fatal("Wrong output for test #%d\n", i)
